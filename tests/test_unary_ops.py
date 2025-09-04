f4933bdfd7bef965377cdd6ca4450cbcdb98ee80
Jason Yoder <jason.yoder2@bsu.edu>
2025-09-03 22:04:54 -0400

    Fixes test bug

--- tests/test_unary_ops.py
+++ tests/test_unary_ops.py
@@ -7,5 +7,5 @@ def test_square_root_with_quinary_input():
     assert unary_ops.square_root("14") == "3"
 
 def test_square_root_edge_case():
-    assert unary_ops.square_root("0") == "0"
+    assert unary_ops.square_root("0") == ""
     assert unary_ops.square_root("1") == "1"
